fd21862fa876898559a1f4064d593f3684bb14ea
Day <dayannefernandesc@gmail.com>
2016-07-06 03:19:36 -0300

    insert ordered secondary index

--- T2/source/gera_index.cpp
+++ T2/source/gera_index.cpp
@@ -18,7 +18,9 @@ class InvertIndex
 public:
   InvertIndex(string f){filename = f;};
   void createII(vector<string>, int);
-  // void insert(string);
+  void insertOrdered(vector<pair<string, vector<int> > > &, int, string);
+  void insertOrderedInvertList(vector<int> &, int);
+  void printVectorSec(ofstream &, vector<int>);
   // void remove(string);
   // void search(string);
   // void print();
@@ -62,17 +64,17 @@ void createFiles(vector<string> files)
   PI2.createPI(files, 2);
   PI3.createPI(files, 3);
 
-  // InvertIndex II1("sec_lista1.txt"), II2("sec_lista2.txt"), II3("sec_lista3.txt");
-  // II1.createII();
-  // II2.createII();
-  // II3.createII();
+  InvertIndex II1("sec_lista1.txt"), II2("sec_lista2.txt"), II3("sec_lista3.txt");
+  II1.createII(files, 1);
+  //II2.createII(files, 2);
+  //II3.createII(files, 3);
 }
 
 void PrimaryIndex::createPI(vector<string> files, int indexFile)
 {
   ofstream index;
   ifstream benchmark;
-  string id, line;
+  string id;
   vector<pair<string,int> > indexList;
   int i=0;
 
@@ -82,6 +84,7 @@ void PrimaryIndex::createPI(vector<string> files, int indexFile)
   if(index.is_open() && benchmark.is_open())
   {
     if(indexFile==1)
+      // static file
       while(benchmark >> id)
       {
         benchmark.ignore(256, '\n');
@@ -90,6 +93,7 @@ void PrimaryIndex::createPI(vector<string> files, int indexFile)
         ++i;
       }
     else
+      // dynamic file
       while(getline(benchmark,id,'|'))
       {
         benchmark.ignore(256, '\n');
@@ -98,6 +102,9 @@ void PrimaryIndex::createPI(vector<string> files, int indexFile)
         ++i;
       }
   }
+
+  // number of deleted registers of the data file
+  index << 0 << endl;
 
   for(int j=0; j<indexList.size(); ++j)
     index << indexList[j].first << " " << indexList[j].second << endl;
@@ -123,7 +130,104 @@ void PrimaryIndex::insertOrdered(vector<pair<string,int> > &indexList, pair<stri
 
 void InvertIndex::createII(vector<string> files, int indexFile)
 {
-  ofstream file;
-  file.open(this->filename, ios::in);
-  file.close();
+  ofstream index;
+  ifstream benchmark;
+  string id, number, name;
+  vector<pair<string, vector<int> > > pkList;
+  vector<string> nameList;
+  int i=0;
+
+  index.open(this->filename);
+  benchmark.open(files[indexFile]);
+
+  if(index.is_open() && benchmark.is_open())
+  {
+    if(indexFile==1)
+      // static file
+      while(benchmark >> id >> number >> name)
+      {
+        benchmark.ignore(256, '\n');
+        // cout << name << endl;
+        // getchar();
+        this->insertOrdered(pkList, i, name);
+        ++i;
+      }
+    // else
+    //   // dynamic file
+    //   while(getline(benchmark,id,'|'))
+    //   {
+    //     benchmark.ignore(256, '\n');
+    //     pair<string,int> p(id,i);
+    //     this->insertOrdered(indexList, p);
+    //     ++i;
+    //   }
+  }
+
+  for(int j=0; j<pkList.size(); ++j)
+  {
+    index << pkList[j].first << " ";
+    this->printVectorSec(index, pkList[j].second);
+    index << endl;
+  }
+
+  index.close();
+  benchmark.close();
+}
+
+void InvertIndex::printVectorSec(ofstream &index, vector<int> pkList)
+{
+  for(int i=0; i<pkList.size(); ++i) index << pkList[i] << " ";
+}
+
+void InvertIndex::insertOrdered(vector<pair<string, vector<int> > > &pkList, int rrn, string name)
+{
+  vector<pair<string, vector<int> > >::iterator it = pkList.begin();
+  pair<string, vector<int> > aux;
+  vector<int> nameList;
+
+  if(pkList.empty())
+  {
+    nameList.push_back(rrn);
+    aux=make_pair(name, nameList);
+    pkList.push_back(aux);
+  }
+  else
+  {
+    while(it!=pkList.end())
+    {
+      if((*it).first!=name) it++;
+      else break;
+    }
+    if(it!=pkList.end())
+    {
+      nameList = (*it).second;
+      this->insertOrderedInvertList(nameList, rrn);
+      (*it).second = nameList;
+    }
+    else
+    {
+      nameList.push_back(rrn);
+      aux=make_pair(name, nameList);
+      pkList.push_back(aux);
+    }
+  }
+
+  // cout << pkList.back().first << endl;
+  // this->printVectorSec(pkList.back().second);
+  // cout << endl;
+  // getchar();
+}
+
+void InvertIndex::insertOrderedInvertList(vector<int> &nameList, int rrn)
+{
+  vector<int>::iterator it = nameList.end()-1;
+  int aux;
+
+  if(*it>rrn)
+  {
+    while(*it>rrn) --it;
+    aux=*it;
+    nameList.insert(it+1,rrn);
+  }
+  else nameList.push_back(rrn);
 }
