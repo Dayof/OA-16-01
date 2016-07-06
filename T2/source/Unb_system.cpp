b44fbfd9b561b7ec89b2db501950717bfde6036c
Day <dayannefernandesc@gmail.com>
2016-07-06 06:00:41 -0300

    delete student and update index file

--- T2/source/Unb_system.cpp
+++ T2/source/Unb_system.cpp
@@ -12,8 +12,15 @@ public:
 
   bool askInfoStudent(vector<string> &);
   void registerNameFile(fstream &, string);
-  string generateLastId(fstream &);
+  string generateLastId();
+  int searchStudentInPkIndex(string);
+  void deleteStudentPkIndex(int);
+
+  void loadPkIndex();
+  void attPkIndex();
 private:
+  vector<pair<string,int> > indexList;
+  int del_reg;
 };
 
 int main( int argc, char *argv[] )
@@ -26,6 +33,8 @@ void showMenu()
     string buffer;
     int op;
     StudentManagement students;
+
+    students.loadPkIndex();
 
     clearScreen();
     cout << "Sistema de Notas UnB v0.1 beta" << endl;
@@ -66,6 +75,31 @@ void showMenu()
 
 }
 
+void StudentManagement::loadPkIndex()
+{
+  ifstream pkIndex;
+  string id;
+  int nrr;
+
+  pkIndex.open("index_lista1.txt");
+
+  if(pkIndex.is_open())
+  {
+    pkIndex >> nrr;
+    pkIndex.ignore(256, '\n');
+    this->del_reg=nrr;
+
+    while(pkIndex >> id >> nrr)
+    {
+      pkIndex.ignore(256, '\n');
+      pair<string,int> p(id,nrr);
+      this->indexList.push_back(p);
+    }
+  }
+
+  pkIndex.close();
+}
+
 void StudentManagement::insertNewStudent()
 {
   fstream index;
@@ -77,12 +111,14 @@ void StudentManagement::insertNewStudent()
     index.open("lista1.txt", ios::in | ios::app);
     if(index.is_open())
     {
-      id = this->generateLastId(index);
+      id = this->generateLastId();
       index << id << " " << infoStudent[0] << " ";
             this->registerNameFile(index, infoStudent[1]);
       index << " " << infoStudent[2] << "    " << infoStudent[3]
             << "         " << infoStudent[4] << endl;
+      cout << "Executando gerador de índices..." << endl;
       system("./gera_index lista1.txt lista2.txt lista3.txt");
+      cout << "Índices atualizados!" << endl;
     }
 
     index.close();
@@ -90,13 +126,12 @@ void StudentManagement::insertNewStudent()
   else showMenu();
 }
 
-string StudentManagement::generateLastId(fstream &index)
+string StudentManagement::generateLastId()
 {
   string id, ad1= "0", ad2= "00";
   int aux;
 
-  index.seekg(-56, ios::end);
-  index >> id;
+  id=this->indexList.back().first;
 
   id.erase(0,2);
   aux = stoi(id);
@@ -165,7 +200,78 @@ bool StudentManagement::askInfoStudent(vector<string> &infoStudent)
 
 void StudentManagement::deleteStudent()
 {
+  string id, ans;
+  bool incorrect;
+  int pos_student;
 
+  clearScreen();
+  do {
+    cout << "Escreva o ID do aluno a ser jubilado:" << endl;
+    cout << "ID (ID000, de 000 a 999)\n";
+    cin >> id;
+    if( id.size()!=5)
+    {
+      incorrect=true;
+      cout << "Dado incorreto, tente inserir novamente." << endl;
+      pressEnter();
+    }
+    else
+    {
+      pos_student=this->searchStudentInPkIndex(id);
+      if(pos_student>=0) incorrect=false;
+      else
+      {
+        cout << "Estudante não existe! Tente inserir outro nome." << endl;
+        pressEnter();
+        incorrect=true;
+      }
+    }
+  } while(incorrect);
+
+  cout << "Deseja jubilar o aluno de ID " << id << " ? (y/n)" << endl;
+  cin >> ans;
+
+  if(ans=="y")
+  {
+    this->deleteStudentPkIndex(pos_student);
+    cout << "Aluno deletado com sucesso." << endl;
+    pressEnter();
+  }
+  showMenu();
+}
+
+void StudentManagement::deleteStudentPkIndex(int pos_student)
+{
+  this->indexList[pos_student].second=-1;
+  ++this->del_reg;
+  this->attPkIndex();
+}
+
+void StudentManagement::attPkIndex()
+{
+  ofstream pkIndex;
+
+  pkIndex.open("index_lista1.txt");
+
+  if(pkIndex.is_open())
+  {
+    pkIndex << this->del_reg << endl;
+
+    for(int j=0; j<this->indexList.size(); ++j)
+      pkIndex << indexList[j].first << " " << indexList[j].second << endl;
+  }
+
+  pkIndex.close();
+}
+
+int StudentManagement::searchStudentInPkIndex(string id)
+{
+  vector<pair<string,int> >::iterator it;
+  for(it=this->indexList.begin(); it!=this->indexList.end();++it)
+    if((*it).first==id) break;
+
+  if(it==this->indexList.end()) return -10000;
+  else return (distance(this->indexList.begin(), it));
 }
 
 void StudentManagement::gradingStudent()
